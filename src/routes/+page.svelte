bd3ca9bce869f6e70a7510e97a4b6578736736cf
Samuel Genest <is.genest@gmail.com>
2023-05-14 18:52:25 -0400

    feat(#47): Created a `<Sidebar />` Component
    Created a `<Sidebar />` Component.
    
    Closes #47

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,9 @@
   
   <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </svelte:head>
+
+<script>
+  import Sidebar from '../lib/components/sidebar/sidebar.svelte'
+</script>
+
+<Sidebar />
