603adc0c31279231a9d393a0f8835ff84e59305a
Samuel Genest <is.genest@gmail.com>
2023-05-15 00:28:20 -0400

    feat(#48): Created a `<CoinTypeSelect />` Component.
    Created a `<CoinTypeSelect />` Component.
    
    Closes #48

---
+++ src/lib/components/inputs/coin-type-select.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let id
+  export let name
+  export let coins
+</script>
+
+<select {name} {id}>
+  {#each coins as coin}
+  <option value={coin.value}>{coin.symbol}</option>
+  {/each}
+</select>
