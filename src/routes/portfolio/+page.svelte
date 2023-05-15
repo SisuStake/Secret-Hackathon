7068e558efe11cb1a04e91875cd79659b9bb6164
Samuel Genest <is.genest@gmail.com>
2023-05-15 15:22:16 -0400

    feat(#82): Added `Portfolio` Header to `/portfolio`
    Added a `<h1>Portfolio</h1>` Header to `/portfolio`
    
    Closes #82

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -4,6 +4,8 @@
   <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </svelte:head>
 
+<h1>Portfolio</h1>
+
 <script>
   import PortfolioTable from '../../lib/components/tables/portfolio.svelte'
 </script>
