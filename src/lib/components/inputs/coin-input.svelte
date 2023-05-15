1999ab8a6830b30b0370f5beca656f6f06b08a85
Samuel Genest <is.genest@gmail.com>
2023-05-15 00:37:35 -0400

    feat(#49): Created a `<CoinValueInput />` Component.
    Created a `<CoinValueInput />` Component.
    
    Closes #49

---
+++ src/lib/components/inputs/coin-input.svelte
@@ -0,0 +1,12 @@
+<script>
+  import CoinTypeSelect from './coin-type-select.svelte'
+  import CoinValueInput from './coin-value-input.svelte'
+
+  export let id
+  export let coins
+</script>
+
+<div {id} class="mb-3">
+  <CoinTypeSelect name={id + '-value'} id={id + '-value'} {coins} />  
+  <CoinValueInput name={id + '-value'} id={id + '-value'} />  
+</div>
---
+++ src/lib/components/inputs/coin-value-input.svelte
@@ -0,0 +1,6 @@
+<script>
+  export let name
+  export let id
+</script>
+
+<input {id} {name} type="number" step="0.01" placeholder="0.00">
